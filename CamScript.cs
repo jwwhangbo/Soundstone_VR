4e4f9b5d3e14fdf5024ab70bca3ae9a6b6a09752
jwwhangbo <jwwhangbo@gmail.com>
2018-07-30 20:15:03 +0900

    Add files via upload

---
+++ CamScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour
+{
+    private PlayerController player;
+	// Use this for initialization
+	void Start ()
+	{
+	    player = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 newCamPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        transform.position = new Vector3(Mathf.Clamp(newCamPos.x, 2, 36), Mathf.Clamp(newCamPos.y,1,6), transform.position.z);
+	}
+}
